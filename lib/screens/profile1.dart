c52b301ee4b8b94d4c24548d9a3e8174b972bf74
TRAGIC11 <78635070+TRAGIC11@users.noreply.github.com>
2021-03-30 15:03:50 +0530

    Initial Profile Page - Piyush

---
+++ lib/screens/profile1.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+
+class ProfilePage extends StatefulWidget {
+  @override
+  _ProfilePageState createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  final TextEditingController _nameController = TextEditingController();
+
+  Widget onPressed() {
+    return AlertDialog(
+      title: Text("Name"),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          TextField(
+            controller: _nameController,
+          )
+        ],
+      ),
+      actions: [
+        TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: Text("Confirm")),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Profile"),
+        backgroundColor: Colors.amber,
+      ),
+      body: Container(
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(
+                height: 100,
+              ),
+              CircleAvatar(
+                radius: 50,
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width - 50,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Text(
+                      _nameController.text == ""
+                          ? "Full Name"
+                          : _nameController.text,
+                      style: TextStyle(
+                        fontSize: 24,
+                      ),
+                    ),
+                    ElevatedButton(
+                        onPressed: () {
+                          onPressed();
+                        },
+                        child: Text("Edit",
+                            style: TextStyle(
+                              fontSize: 24,
+                            ))),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
