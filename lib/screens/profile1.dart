54e79a35669a8a33b54ebd9c87c43f3fb1540702
TRAGIC11 <piyush.bhujbal0201@gmail.com>
2021-04-03 01:55:36 +0530

    Profile1 - Edit 3

--- lib/screens/profile1.dart
+++ lib/screens/profile1.dart
@@ -49,10 +49,29 @@ class _ProfileState extends State<Profile> {
             crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
               SizedBox(
-                height: 80,
+                height: 70,
               ),
               CircleAvatar(
+                backgroundColor: Colors.grey[400],
+                backgroundImage: NetworkImage(
+                    'https://images.unsplash.com/photo-1459802071246-377c0346da93?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1795&q=80'),
                 radius: 80,
+              ),
+              Positioned(
+                child: SizedBox(
+                  child: FlatButton(
+                    color: Colors.green[600],
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(80),
+                      side: BorderSide(color: Colors.red),
+                    ),
+                    child: Text("Image"),
+                    /*child: Image(
+                      image: AssetImage(null),
+                    ),*/
+                    onPressed: null,
+                  ),
+                ),
               ),
               SizedBox(
                 height: 60,
